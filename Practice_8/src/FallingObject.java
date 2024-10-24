054984272d5cd1301502e5a2d3ed6a37609d5e74
Antes <coderasylum@gmail.com>
2024-10-24 11:03:10 +0300

    feat: add 8-th practice (option №4)

---
+++ Practice_8/src/FallingObject.java
@@ -0,0 +1,32 @@
+class FallingObject {
+    private double height;
+    private double force;
+    private double lift;
+    private double resistance;
+    private double speed = 0;
+    private double time = 0;
+
+    public FallingObject(double height, double force, double lift, double resistance) {
+        this.height = height;
+        this.force = force;
+        this.lift = lift;
+        this.resistance = resistance;
+    }
+
+    public synchronized boolean updatePosition(double deltaTime) {
+        double acceleration = force - lift - resistance;
+        speed += acceleration * deltaTime;
+        height -= speed * deltaTime;
+        time += deltaTime;
+
+        return height <= 0;  // возвращает true, если объект достиг земли
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public double getDistance() {
+        return Math.max(0, height);
+    }
+}
---
+++ Practice_8/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+    public static void main(String[] args) {
+        double startHeight = round(100.0, 2);
+        double moveForce = round(9.8,2);
+        double liftForce = round(2.0, 2);
+        double resistanceForce = round(1.0,2);
+
+        if (moveForce <= round(liftForce + resistanceForce,2)) {
+            System.out.println("Сила, движущая вниз должна быть больше сил, движущих вверх.");
+            return;
+        }
+        FallingObject object = new FallingObject(startHeight, moveForce, liftForce, resistanceForce);
+        SimulationThread simulation = new SimulationThread(object);
+        simulation.start();
+    }
+}
